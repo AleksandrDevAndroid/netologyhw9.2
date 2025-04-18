54ba45ac84ca27f911e525987dc573065bb7c3b6
AleksandrDevAndroid <aleksperdev@gmail.com>
2025-04-18 14:20:11 +0300

    Add files via upload

---
+++ SmartLogger.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public interface SmartLogger {
+    AtomicInteger callCounter = new AtomicInteger(0);
+
+
+    static void log(String message) {
+        int currentCount = callCounter.incrementAndGet();
+
+        if (message.toLowerCase().equals("error")) {
+            System.out.println("ERROR");
+        } else
+            System.out.printf("INFO#%d %s %s\n", currentCount, LocalDateTime.now(), message);
+
+    }
+}
