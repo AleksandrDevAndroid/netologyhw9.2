cd65d0f5e40f87844fd7127e08b3be770b335fed
AleksandrDevAndroid <aleksperdev@gmail.com>
2025-04-18 14:19:33 +0300

    Delete SimpleLogger.java

--- SimpleLogger.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDateTime;
-
-public interface SimpleLogger {
-    static void log(String message) {
-        System.out.printf("%s %s\n",LocalDateTime.now(),message);
-    }
-}
